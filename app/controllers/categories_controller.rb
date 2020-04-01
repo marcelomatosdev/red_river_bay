e96eb13d559581f4fe973653d374de8b4907a02c
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-04-01 12:14:32 -0500

    Added categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get '/categories', to: 'categories#index'
+  get '/categories/:id', to: 'categories#show', id: /\d+/, as: 'category'
+  get 'categories/index'
+  get 'categories/show'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
