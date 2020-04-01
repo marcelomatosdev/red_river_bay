1173ddeedbe2cd36ffb0162a3f2d59b1b5db4f65
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-04-01 12:44:50 -0500

    Added Product model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+  # get 'products/index'
+  # get 'products/show'
+  # get 'users/index'
+  # get 'users/show'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', id: /\d+/, as: 'user'
   get '/categories', to: 'categories#index'
   get '/categories/:id', to: 'categories#show', id: /\d+/, as: 'category'
-  get 'categories/index'
-  get 'categories/show'
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', id: /\d+/, as: 'product'
+  # get 'categories/index'
+  # get 'categories/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_171814) do
+ActiveRecord::Schema.define(version: 2020_04_01_173346) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 2020_04_01_171814) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "price"
+    t.string "quantity"
+    t.integer "user_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -53,4 +66,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_171814) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "users"
 end
