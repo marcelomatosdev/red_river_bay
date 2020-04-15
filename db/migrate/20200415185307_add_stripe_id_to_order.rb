299e209b789a48c8c452776d500feb2b2158207a
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-04-15 14:01:03 -0500

    added column stripe_id

---
+++ db/migrate/20200415185307_add_stripe_id_to_order.rb
@@ -0,0 +1,5 @@
+class AddStripeIdToOrder < ActiveRecord::Migration[6.0]
+  def change
+    add_column :orders, :stripe_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_225615) do
+ActiveRecord::Schema.define(version: 2020_04_15_185307) do
 
   create_table "abouts", force: :cascade do |t|
     t.string "title"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_225615) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
